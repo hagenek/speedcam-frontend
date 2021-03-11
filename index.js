ccfb955949cdb68102de5ec2c6bc17499ce1e1d7
Georg H. Ekeberg <georghagen@gmail.com>
2021-03-11 14:46:04 +0100

    added unique text on popup

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const headLine = (document.querySelector("h1").style = "color: blue");
+const { token } = require("env.js");
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaGFnZW5layIsImEiOiJja2JveGdsZ3Mxam91MnFxbnJua3Nmamg4In0.5smhwVMhZaL70x3KvQ_qCw";
+
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+});
+
+map.addControl(new mapboxgl.NavigationControl());
 
 const getGeoJson = async (uri) => {
   const geojson = await fetch(uri)
@@ -10,8 +21,16 @@ const getGeoJson = async (uri) => {
     var el = document.createElement("div");
     el.className = "marker";
 
+    // Popup with custom text from the JSON
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      marker.properties.label.text
+    );
+
     // make a marker for each feature and add to the map
-    new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+    new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .setPopup(popup)
+      .addTo(map); // sets a popup on this marker
   });
 };
 
